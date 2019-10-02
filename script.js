1fb1879f00a29559c9aa497044552bf991e86d23
kwlahr <kwlahr@gmail.com>
2019-10-02 16:24:47 -0600

    added slider functionality in script.js

--- script.js
+++ script.js
@@ -10,6 +10,32 @@ function generate(){
     //for loop related to password length
     for(var i = 0; i <= complexity; i++){
 
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)))
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)))
+    }
+
+    //add password to text box
+    document.getElementById("display").value = password;
+
+    //password history
+    document.getElementById("last-passwords").innerHTML += password + "<br>"
+}
+
+//set default length display of 10
+document.getElementById("length").innerHTML = "Length: 10";
+
+//var for slider value
+
+//function to set length to slider position
+document.getElementById("slider").oninput = function(){
+    var sliderValue = document.getElementById("slider").value -1;
+
+    if(document.getElementById("slider").value > 0){
+    document.getElementById("length").innerHTML = "Length: " + sliderValue;
+    //document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{ 
+    document.getElementById("length").innerHTML = "Length: 1";
     }
 }
+
+//copy to clipboard function
