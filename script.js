ee0c0661913f6a009ab4e81d2a8a8dbbd90d9691
kwlahr <kwlahr@gmail.com>
2019-10-02 15:50:36 -0600

    added function for password generation to script.js

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ function generate(){
     //set length/complexity
     let complexity = document.getElementById("slider").value; 
 
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+    let password = "";
+
+    //for loop related to password length
+    for(var i = 0; i <= complexity; i++){
+
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)))
+    }
 }
