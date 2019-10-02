2dbb01d3914e2a3f753bcb454ed9e7056b74aafc
kwlahr <kwlahr@gmail.com>
2019-10-02 15:42:53 -0600

    added styles to style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+function generate(){
+
+    //set length/complexity
+    let complexity = document.getElementById("slider").value; 
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+}
